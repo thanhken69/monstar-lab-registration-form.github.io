0bdf6b5df0728f58994bb44be3019582ae5c36eb
Thành Ken <thanhken69@gmail.com>
2021-06-29 17:41:09 +0700

    change js

--- main.js
+++ main.js
@@ -1,150 +1,107 @@
 const inputRegister = document.querySelectorAll('.register__input'),
     message = document.querySelectorAll('.register__message'),
-    nameRegister = document.getElementById('register__name'),
-    submit = document.querySelector('.register__submit'),
-    loading = document.querySelector('.register__loading'),
-    success = document.querySelector('.register__success'),
-    close = document.querySelector('.register__close'),
-    overlay = document.querySelector('.overlay');
+    submit = document.querySelector('#registerSubmit'),
+    loading = document.querySelector('#registerLoading'),
+    success = document.querySelector('#registerModalSuccess'),
+    close = document.querySelector('#registerClose'),
+    overlay = document.querySelector('#registerOverlay');
 
-for (let i = 0; i < inputRegister.length; i++) {
-    inputRegister[i].addEventListener('click', function () {
-        message[i].style.visibility = 'visible';
-
-        if (isValid(inputRegister[0].value)) {
-            message[0].style.visibility = 'hidden';
-        }
-        if (validateEmail(inputRegister[1].value)) {
-            message[1].style.visibility = 'hidden';
-        }
-        if (validatePassword(inputRegister[2].value)) {
-            message[2].style.visibility = 'hidden';
-        }
-        if (inputRegister[3].value === inputRegister[2].value && !isEmpty(inputRegister[3].value)) {
-            message[3].style.visibility = 'hidden';
-        }
-    })
+let nameError = {
+    notEmty: 'Tên không được bỏ trống.',
+    noSpecialCharacters: 'Tên không được có ký tự đặc biệt.'
 }
 
-for (let i = 0; i < inputRegister.length; i++) {
-    inputRegister[i].addEventListener('keyup', function () {
-        if (checkSubmit(inputRegister)) {
-            submit.classList.remove('register__submit--disabel');
-            submit.classList.add('register__submit--enable');
-        } else {
-            submit.classList.add('register__submit--disabel');
-            submit.classList.remove('register__submit--enable');
-        }
-    })
+let emailError = {
+    notEmty: 'Email không được bỏ trống.',
+    noSpecialCharacters: 'Sai cú pháp email.'
 }
 
-inputRegister[0].addEventListener('change', function () {
-    if (isEmpty(inputRegister[0].value) || inputRegister[0].value === null || inputRegister[0] === undefined) {
-        message[0].textContent = 'Tên không được bỏ trống.';
-        inputRegister[0].style.borderColor = 'red';
-        message[0].style.color = 'red';
-        message[0].style.visibility = 'visible';
-    } else if (!isValid(inputRegister[0].value)) {
-        inputRegister[0].style.borderColor = 'red';
-        message[0].style.color = 'red';
-        message[0].style.visibility = 'visible';
-        message[0].textContent = 'Tên không được có ký tự đặc biệt.';
-    } else {
-        inputRegister[0].style.borderColor = 'black';
-        message[0].style.visibility = 'hidden';
-    }
-})
+let passwordError = {
+    notEmty: 'Mật khẩu không được bỏ trống',
+    noSpecialCharacters: 'Mật khẩu 8-32 ký tự bao gồm số, chữ thường, chữ hoa, ký tự đặc biệt.'
+}
 
-inputRegister[1].addEventListener('change', function () {
-    if (isEmpty(inputRegister[1].value) || inputRegister[1].value === null || inputRegister[1] === undefined) {
-        message[1].textContent = 'Email không được bỏ trống.';
-        inputRegister[1].style.borderColor = 'red';
-        message[1].style.color = 'red';
-        message[1].style.visibility = 'visible';
-    } else if (validateEmail(inputRegister[1].value)) {
-        message[1].style.visibility = 'hidden';
-        inputRegister[1].style.borderColor = 'black';
-        message[1].style.color = 'black';
-    } else {
-        inputRegister[1].style.borderColor = 'red';
-        message[1].textContent = 'Sai cú pháp email.';
-        message[1].style.color = 'red';
-        message[1].style.visibility = 'visible';
-    }
-})
+validateField(0, nameError, isValid);
+validateField(1, emailError, validateEmail);
+validateField(2, passwordError, validatePassword);
 
 inputRegister[2].addEventListener('change', function () {
-    if (isEmpty(inputRegister[2].value) || inputRegister[2].value === null || inputRegister[2] === undefined) {
-        message[2].textContent = 'Mật khẩu không được bỏ trống.';
-        inputRegister[2].style.borderColor = 'red';
-        message[2].style.color = 'red';
-        message[2].style.visibility = 'visible';
-    } else if (validatePassword(inputRegister[2].value)) {
-        message[2].style.visibility = 'hidden';
-        inputRegister[2].style.borderColor = 'black';
-        message[2].style.color = 'black';
-    } else {
-        message[2].textContent = 'Mật khẩu 8-32 ký tự bao gồm số, chữ thường, chữ hoa, ký tự đặc biệt.';
-        inputRegister[2].style.borderColor = 'red';
-        message[2].style.color = 'red';
-        message[2].style.visibility = 'visible';
-    }
-    if (inputRegister[3].value === inputRegister[2].value) {
-        message[3].style.visibility = 'hidden';
-        inputRegister[3].style.borderColor = 'black';
-        message[3].style.color = 'black';
-    } else {
-        inputRegister[3].style.borderColor = 'red';
-        message[3].style.color = 'red';
-        message[3].style.visibility = 'visible';
-    }
+    confirmPassword();
 })
 
 inputRegister[3].addEventListener('change', function () {
-    if (inputRegister[3].value === inputRegister[2].value) {
-        message[3].style.visibility = 'hidden';
-        inputRegister[3].style.borderColor = 'black';
-        message[3].style.color = 'black';
-    } else {
-        inputRegister[3].style.borderColor = 'red';
-        message[3].style.color = 'red';
-        message[3].style.visibility = 'visible';
-    }
+    confirmPassword();
 })
 
-
-submit.addEventListener('click', function () {
-    if (checkSubmit(inputRegister)) {
-        submit.classList.add('register__submit--disabel');
-        submit.classList.remove('register__submit--enable');
-        loading.style.display = 'inline-block';
-    }
-
-    setTimeout(function () {
-        if (checkSubmit(inputRegister)) {
-            loading.style.display = 'none';
-            success.style.display = 'block';
-            overlay.style.display = 'block';
-
-            submit.classList.add('register__submit--disabel');
-            submit.classList.remove('register__submit--enable');
-
-            for (let i = 0; i < inputRegister.length; i++) {
-                inputRegister[i].value = ''
-            }
-        }
-    }, 2000)
-})
-
-
-submit.addEventListener('click', function () {
-
-})
+submitForm();
 
 close.addEventListener('click', function () {
     success.style.display = 'none';
     overlay.style.display = 'none';
 })
+
+function validateField(element, error, validateElement) {
+    inputRegister[element].addEventListener('change', function () {
+        if (isEmpty(inputRegister[element].value) || inputRegister[element].value === null || inputRegister[element] === undefined) {
+            message[element].textContent = error.notEmty;
+            inputRegister[element].style.borderColor = 'red';
+            message[element].style.color = 'red';
+            message[element].style.visibility = 'visible';
+        } else if (!validateElement(inputRegister[element].value)) {
+            inputRegister[element].style.borderColor = 'red';
+            message[element].style.color = 'red';
+            message[element].style.visibility = 'visible';
+            message[element].textContent = error.noSpecialCharacters;
+        } else {
+            inputRegister[element].style.borderColor = 'black';
+            message[element].style.visibility = 'hidden';
+        }
+    })
+}
+
+function confirmPassword() {
+    if (inputRegister[3].value === inputRegister[2].value) {
+        message[3].style.visibility = 'hidden';
+        inputRegister[3].style.borderColor = 'black';
+        message[3].style.color = 'black';
+    } else {
+        inputRegister[3].style.borderColor = 'red';
+        message[3].style.color = 'red';
+        message[3].style.visibility = 'visible';
+    }
+}
+
+function submitForm() {
+    for (let i = 0; i < inputRegister.length; i++) {
+        inputRegister[i].addEventListener('keyup', function () {
+            if (checkSubmit(inputRegister)) {
+                submit.classList.remove('register__submit--disabel');
+                submit.classList.add('register__submit--enable');
+            } else {
+                submit.classList.add('register__submit--disabel');
+                submit.classList.remove('register__submit--enable');
+            }
+        })
+    }
+
+    submit.addEventListener('click', function () {
+        if (checkSubmit(inputRegister)) {
+            submit.classList.add('register__submit--disabel');
+            submit.classList.remove('register__submit--enable');
+            loading.style.display = 'inline-block';
+
+            setTimeout(function () {
+                loading.style.display = 'none';
+                success.style.display = 'block';
+                overlay.style.display = 'block';
+
+                for (let i = 0; i < inputRegister.length; i++) {
+                    inputRegister[i].value = ''
+                }
+            }, 2000)
+        }
+    })
+}
 
 function removeAscent(str) {
     if (str === null || str === undefined) return str;
@@ -160,7 +117,7 @@ function removeAscent(str) {
 }
 
 function isValid(string) {
-    var re = /^[a-zA-Z ]{2,}$/g;
+    const re = /^[a-zA-Z ]{2,}$/g;
     return re.test(removeAscent(string))
 }
 
@@ -176,7 +133,6 @@ function validatePassword(password) {
 
 function isEmpty(str) {
     return (!str || 0 === str.length);
-    // return (!str || /^\s*$/.test(str));
 }
 
 function checkSubmit(inputRegister) {
